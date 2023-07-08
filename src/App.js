f3271b45981492743e43f5408dc7776994edd91f
Alxrdp <alexrdphc@gmail.com>
2023-07-07 23:22:21 -0500

    Primera preEntrega reactjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/Navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
+  <header className="headerPatriota">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <ItemListContainer greeting={'Bienvenidos'}/>
     </div>
+    </header>
   );
 }
 
